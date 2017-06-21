33febe2375a15f722622b36a32186c02db028d8d
Ricardo <danmleal@gmail.com>
2017-06-21 15:33:49 -0500

    Readme Usuarios

--- README.md
+++ README.md
@@ -3,44 +3,66 @@
 Orchextra SDK para el consumo del módulo de Genreación.
 
 ### Instalación (npm)
-
+```js
 npm install orchextra-client-generation
-
+```
 ## Ejemplos
 
 ### Usuarios
+```js
 var userApi = require('orchextra-client-generation/users');
-
+```
 #### Listado de Usuarios
+```js
 userApi.getUsers(token, url).then(users => {
   users.forEach((user) => console.log(user.id));
 })
 .catch(error => {
   console.log(error);
 });
+```
 #### Información de Usuario
+```js
 userApi.getUser(token, url,idUser).then(user => {
   console.log(user);
 })
 .catch(error => {
   console.log(error);
 });
+```
 #### Crear Usuario
+```js
+var user = {
+  name: 'Daniel',
+  email: 'daniel@gigigo.com.mx',
+  username: 'Daniel',
+  role: 'Role',
+  password: 'gigigo',
+  passwordConfirmation: 'gigigo',
+  languageCode: 'es',
+  projectsIds: ["5936cb98d318c404f94951e2"]
+};
+
 userApi.createUser(token, url, user).then(user => {
   console.log(user);
 }).catch(error => {
   console.log(error);
 });
+```
 #### Eliminar Usuario
+```js
 userApi.deleteUser(token, url,idUser).then(user => {
   console.log(user);
 })
 .catch(error => {
   console.log(error);
 });
+```
 #### Reemplazar Usuario
+```js
 userApi.replaceUser(token, url,idUser, user).then(user => {
   console.log(user);
 }).catch(error => {
   console.log(error);
 });
+```
--- test/userTest.js
+++ test/userTest.js
@@ -4,50 +4,51 @@ var auth = new Auth('https://auth-api-coupons.s.gigigoapps.com')
 
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNJZCI6IjU5MmVlODQxYjQ2YzJjYmRjMmFjZTU4ZCIsIm5hbWUiOiJEYXNoYm9hcmQiLCJ0eXBlIjoiZ2VuZXJhdGlvbiIsImxhc3RSZXF1ZXN0IjoiMjAxNy0wNi0xNFQyMToxNzowMS44NDdaIiwiaWF0IjoxNDk3NDc1MDIxfQ.VVUEnTb0s0cw-X4hTmOj4t822LkyGnlhAeOUKUBEikI';
 var url = 'https://generation-api-coupons.s.gigigoapps.com';
-const idUser = '5942fdfb3157b629aab3eae8';
-
-
-  // userApi.getUsers(token, url).then(users => {
-  //   users.forEach((user) => console.log(user.id));
-  // })
-  // .catch(error => {
-  //   console.log(error);
-  // });
-
-
-    // userApi.getUser(token, url,idUser).then(user => {
-    //   console.log(user);
-    // })
-    // .catch(error => {
-    //   console.log(error);
-    // });
+const idUser = '5942f7c73157b629aab3eae4';
 
 
 var user = {
-  name: 'userName2',
-  email: 'user2@gigigo.com.mx',
-  username: 'username',
-  role: 'userRole2',
-  password: 'gigigo10',
-  passwordConfirmation: 'gigigo10',
+  name: 'Daniel',
+  email: 'daniel@gigigo.com.mx',
+  username: 'Daniel',
+  role: 'Role',
+  password: 'gigigo',
+  passwordConfirmation: 'gigigo',
   languageCode: 'es',
   projectsIds: ["5936cb98d318c404f94951e2"]
 };
-    userApi.createUser(token, url, user).then(user => {
-      console.log(user);
-    }).catch(error => {
-      console.log(error);
-    });
 
-    // userApi.deleteUser(token, url,idUser).then(user => {
-    //   console.log(user);
-    // })
-    // .catch(error => {
-    //   console.log(error);
-    // });
+userApi.getUsers(token, url).then(users => {
+  users.forEach((user) => console.log(user.id));
+})
+.catch(error => {
+  console.log(error);
+});
 
-      // userApi.replaceUser(token, url,idUser, user).then(user => {
-      //   console.log(user);
-      // }).catch(error => {
-      //   console.log(error);
-      // });
+
+userApi.getUser(token, url,idUser).then(user => {
+  console.log(user);
+})
+.catch(error => {
+  console.log(error);
+});
+
+
+userApi.createUser(token, url, user).then(user => {
+  console.log(user);
+}).catch(error => {
+  console.log(error);
+});
+
+userApi.deleteUser(token, url,idUser).then(user => {
+  console.log(user);
+})
+.catch(error => {
+  console.log(error);
+});
+
+userApi.replaceUser(token, url,idUser, user).then(user => {
+  console.log(user);
+}).catch(error => {
+  console.log(error);
+});
